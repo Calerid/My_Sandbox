8f26592bde8baa49caca48de54b26f7a47681805
Michael Braley <michael.parker.braley@gmail.com>
2022-07-30 11:48:12 -0500

    Add outline for function
    Created a general outline for what I think the function should look like. This is a WIP commit which will need to be fixed at a later time.

--- option_menu/script.js
+++ option_menu/script.js
@@ -1,2 +1,18 @@
 const menuSelection = document.querySelector('select');
 const deploymentContainer = document.getElementById('opt-deploy-container')
+
+menuSelection.addEventListener('change', deployGridBySelection());
+
+function cleanGrid() {
+    deploymentContainer.innerHTML = "";
+    deployGridBySelection();
+}
+
+function deployGridBySelection() {
+    const gridAmount = menuSelection.value;
+    for (let i = 0; i < gridAmount; i++){
+        console.log(i)
+        const newDiv = document.createElement('div');
+    }
+    deploymentContainer.appendChild(newDiv);
+}
